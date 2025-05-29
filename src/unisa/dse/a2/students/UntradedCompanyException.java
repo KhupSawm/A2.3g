e9866b71f46acea139dc73388e73f6c54a2991d2
KhupSawm <165230616+KhupSawm@users.noreply.github.com>
2025-05-29 19:40:30 +0930

    Implement UntradedCompanyException: extend Exception and set message "<code> is not a listed company on this exchange".

--- src/unisa/dse/a2/students/UntradedCompanyException.java
+++ src/unisa/dse/a2/students/UntradedCompanyException.java
@@ -1,8 +1,14 @@
 package unisa.dse.a2.students;
 
-public class UntradedCompanyException
+public class UntradedCompanyException extends Exception
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8308062303527910238L; // Used Eclips to generate serial No.
+
 	public UntradedCompanyException(String companyCode)
 	{
+		super(companyCode + " is not a listed company on this exchange");
 	}
 }
